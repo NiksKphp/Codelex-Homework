250913b0f484d2922a3aa69022ba01b87174aac6
NiksKphp <111875059+NiksKphp@users.noreply.github.com>
2022-11-03 12:55:31 +0200

    mazs labojums

--- Homework: 01.11.2022/02-arrays/05.php
+++ Homework: 01.11.2022/02-arrays/05.php
@@ -3,8 +3,8 @@
 function display_board($gameBoard)
 {
     echo " | {$gameBoard[0][0]} | {$gameBoard[0][1]} | {$gameBoard[0][2]}";
-    echo PHP_EOL . " __________";
-    echo PHP_EOL . " | {$gameBoard[1][0]} | {$gameBoard[1][1]} | {$gameBoard[1][2]}";
+    echo PHP_EOL . " __________" . PHP_EOL;
+    echo " | {$gameBoard[1][0]} | {$gameBoard[1][1]} | {$gameBoard[1][2]}";
     echo PHP_EOL . " __________" . PHP_EOL;
     echo " | {$gameBoard[2][0]} | {$gameBoard[2][1]} | {$gameBoard[2][2]}";
     echo PHP_EOL;
