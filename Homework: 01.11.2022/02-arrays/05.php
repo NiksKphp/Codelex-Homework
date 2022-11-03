d90908b4af2135d2d61e33fb09e57afff46c8fc5
NiksKphp <111875059+NiksKphp@users.noreply.github.com>
2022-11-03 12:24:56 +0200

    mazs labojums

--- Homework: 01.11.2022/02-arrays/05.php
+++ Homework: 01.11.2022/02-arrays/05.php
@@ -1,66 +1,70 @@
 <?php
 
+function display_board($gameBoard)
+{
+    echo " | {$gameBoard[0][0]} | {$gameBoard[0][1]} | {$gameBoard[0][2]}";
+    echo PHP_EOL . " __________";
+    echo PHP_EOL . " | {$gameBoard[1][0]} | {$gameBoard[1][1]} | {$gameBoard[1][2]}";
+    echo PHP_EOL . " __________" . PHP_EOL;
+    echo " | {$gameBoard[2][0]} | {$gameBoard[2][1]} | {$gameBoard[2][2]}";
+    echo PHP_EOL;
 
-function display_board($row1,$row2,$row3){
-    $separator = "---+---+---";
-    echo $row1 . PHP_EOL;
-    echo $separator . PHP_EOL;
-    echo $row2 . PHP_EOL;
-    echo $separator . PHP_EOL;
-    echo $row3 . PHP_EOL;
 
     // Victory conditions;
     //Rows
-    if (($row1[1]=="X")&&($row1[5]=="X")&&($row1[9]=="X")||($row1[1]=="O")&&($row1[5]=="O")&&($row1[9]=="O")) {
+    if ((($gameBoard[0][0] == "X") && ($gameBoard[0][1] == "X") && ($gameBoard[0][2] == "X")) || (($gameBoard[0][0] == "O") && ($gameBoard[0][1] == "O") && ($gameBoard[0][2] == "O"))) {
         echo "Victory for player with last turn" . PHP_EOL;
         exit;
     }
-    if (($row2[1]=="X")&&($row2[5]=="X")&&($row2[9]=="X")||($row2[1]=="O")&&($row2[5]=="O")&&($row2[9]=="O")) {
+    if (($gameBoard[1][0] == "X") && ($gameBoard[1][1] == "X") && ($gameBoard[1][2] == "X") || ($gameBoard[1][0] == "O") && ($gameBoard[1][1] == "O") && ($gameBoard[1][2] == "O")) {
         echo "Victory for player with last turn" . PHP_EOL;
         exit;
     }
-    if (($row3[1]=="X")&&($row3[5]=="X")&&($row3[9]=="X")||($row3[1]=="O")&&($row3[5]=="O")&&($row3[9]=="O")) {
+    if (($gameBoard[2][0] == "X") && ($gameBoard[2][1] == "X") && ($gameBoard[2][2] == "X") || ($gameBoard[2][0] == "O") && ($gameBoard[2][1] == "O") && ($gameBoard[2][2] == "O")) {
         echo "Victory for player with last turn" . PHP_EOL;
         exit;
     }
     //Columns
-    if (($row1[1]=="X")&&($row2[1]=="X")&&($row3[1]=="X")||($row1[1]=="O")&&($row2[1]=="O")&&($row3[1]=="O")) {
+    if (($gameBoard[0][0]=="X")&&($gameBoard[1][0]=="X")&&($gameBoard[2][0]=="X")||($gameBoard[0][0]=="O")&&($gameBoard[1][0]=="O")&&($gameBoard[2][0]=="O")) {
         echo "Victory for player with last turn" . PHP_EOL;
         exit;
     }
-    if (($row1[5]=="X")&&($row2[5]=="X")&&($row3[5]=="X")||($row1[5]=="O")&&($row2[5]=="O")&&($row3[5]=="O")) {
+    if (($gameBoard[0][1]=="X")&&($gameBoard[1][1]=="X")&&($gameBoard[2][1]=="X")||($gameBoard[0][1]=="O")&&($gameBoard[1][1]=="O")&&($gameBoard[2][1]=="O")) {
         echo "Victory for player with last turn" . PHP_EOL;
         exit;
     }
-    if (($row1[9]=="X")&&($row2[9]=="X")&&($row3[9]=="X")||($row1[9]=="O")&&($row2[9]=="O")&&($row3[9]=="O")) {
+    if (($gameBoard[0][2]=="X")&&($gameBoard[1][2]=="X")&&($gameBoard[2][2]=="X")||($gameBoard[0][2]=="O")&&($gameBoard[1][2]=="O")&&($gameBoard[2][2]=="O")) {
         echo "Victory for player with last turn" . PHP_EOL;
         exit;
     }
     // Diagonals
-    if (($row1[1]=="X")&&($row2[5]=="X")&&($row3[9]=="X")||($row1[1]=="O")&&($row2[5]=="O")&&($row3[9]=="O")) {
+    if (($gameBoard[0][0] == "X") && ($gameBoard[1][1] == "X") && ($gameBoard[2][2] == "X") || ($gameBoard[0][0] == "O") && ($gameBoard[1][1] == "O") && ($gameBoard[2][2] == "O")) {
         echo "Victory for player with last turn" . PHP_EOL;
         exit;
     }
-    if (($row1[9]=="X")&&($row2[5]=="X")&&($row3[1]=="X")||($row1[9]=="O")&&($row2[5]=="O")&&($row3[1]=="O")) {
+    if (($gameBoard[0][2] == "X") && ($gameBoard[1][1] == "X") && ($gameBoard[2][0] == "X") || ($gameBoard[0][2] == "O") && ($gameBoard[1][1] == "O") && ($gameBoard[2][0] == "O")) {
         echo "Victory for player with last turn" . PHP_EOL;
         exit;
     }
-
-    if (!($row1[1]==" ")&&!($row1[5]==" ")&&!($row1[9]==" ")&&!($row2[1]==" ")&&!($row2[5]==" ")&&!($row2[9]==" ")&&!($row3[1]==" ")&&!($row3[5]==" ")&&!($row3[9]==" ")){
-        echo "Tie" . PHP_EOL;
-        exit;
-    }
-
+}
+//Tie
+if (in_array(" ",$gameBoard)){
+    echo "Tie" . PHP_EOL;
+    exit;
 }
 
 $gameEnd = false;
-$row1 = "   |   |   ";
-$row2 = "   |   |   ";
-$row3 = "   |   |   ";
+
+$gameBoard = [
+    [' ',' ',' '],
+    [' ',' ',' '],
+    [' ',' ',' '],
+];
 
 $isXturn = true;
 
 while (true) {
+
     $choice = [];
     if ($isXturn == false){
         $choice[] = readline("'O', choose your location (row, column): ");
@@ -71,69 +75,69 @@ while (true) {
     // ROW 1
     if($choice[0][0]==="1"){
         if($choice[0][2] == "1") {
-            $row1[1] = "X";
+            $gameBoard[0][0] = "X";
         }
         if ($choice[0][2] == "1" && !$isXturn) {
-            $row1[1]="O";
+            $gameBoard[0][0]="O";
         }
         if($choice[0][2] == "2"){
-            $row1[5]="X";
+            $gameBoard[0][1]="X";
         }
         if ($choice[0][2] == "2" && !$isXturn) {
-            $row1[5]="O";
+            $gameBoard[0][1]="O";
         }
         if($choice[0][2] == "3"){
-            $row1[9]="X";
+            $gameBoard[0][2]="X";
         }
         if ($choice[0][2] == "3" && !$isXturn) {
-            $row1[9]="O";
+            $gameBoard[0][2]="O";
         }
     }
     // ROW 2
     if($choice[0][0]=="2"){
         if($choice[0][2] == "1") {
-            $row2[1] = "X";
+            $gameBoard[1][0] = "X";
         }
         if ($choice[0][2] == "1" && !$isXturn) {
-            $row2[1]="O";
+            $gameBoard[1][0] ="O";
         }
         if($choice[0][2] == "2"){
-            $row2[5]="X";
+            $gameBoard[1][1] ="X";
         }
         if ($choice[0][2] == "2" && !$isXturn) {
-            $row2[5]="O";
+            $gameBoard[1][1] ="O";
         }
         if($choice[0][2] == "3"){
-            $row2[9]="X";
+            $gameBoard[1][2] ="X";
         }
         if ($choice[0][2] == "3" && !$isXturn) {
-            $row2[9]="O";
+            $gameBoard[1][2] ="O";
         }
     }
 
     // ROW 3
     if($choice[0][0]=="3"){
         if($choice[0][2] == "1") {
-            $row3[1] = "X";
+            $gameBoard[2][0] = "X";
         }
         if ($choice[0][2] == "1" && !$isXturn) {
-            $row3[1]="O";
+            $gameBoard[2][0] ="O";
         }
         if($choice[0][2] == "2"){
-            $row3[5]="X";
+            $gameBoard[2][1] ="X";
         }
         if ($choice[0][2] == "2" && !$isXturn) {
-            $row3[5]="O";
+            $gameBoard[2][1] ="O";
         }
         if($choice[0][2] == "3"){
-            $row3[9]="X";
+            $gameBoard[2][2] ="X";
         }
         if ($choice[0][2] == "3" && !$isXturn) {
-            $row3[9]="O";
+            $gameBoard[2][2] ="O";
         }
     }
 
-    display_board($row1,$row2,$row3);
+    display_board($gameBoard);
 
     // Whose turn
     if ($isXturn){
