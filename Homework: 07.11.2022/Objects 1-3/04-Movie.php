d70be1cc66b56d81be0e658b57c1dfe92910a83f
NiksKphp <111875059+NiksKphp@users.noreply.github.com>
2022-11-09 17:05:58 +0200

    mājasdarbs

--- Homework: 07.11.2022/Objects 1-3/04-Movie.php
+++ Homework: 07.11.2022/Objects 1-3/04-Movie.php
@@ -4,6 +4,7 @@ class Movie {
     private string $title;
     private string $studio;
     private string $rating;
+    private string $rating;
 
     public function __construct(string $title, string $studio, string $rating){
         $this->title = $title;
@@ -15,15 +16,14 @@ class Movie {
         $return = [];
         for ($i=0;$i<count($movie);$i++){
             if ($movie[$i]->rating=="PG"){
-                $return = array_push($return,$movie[$i]);
-                return $return;
+                return array_push($return,$movie[$i]);
             }
         }
     }
 }
 
-$CasinoRoyale = new Movie("Casino Royale","Eon Productions","PG­13");
-$Glass = new Movie("Glass","Buena Vista International","PG­13");
+$CasinoRoyale = new Movie("Casino Royale","Eon Productions","PGSHY13");
+$Glass = new Movie("Glass","Buena Vista International","PGSHY13");
 $SpiderMan = new Movie("Spider-Man: Into the Spider-Verse","Columbia Pictures","PG");
 
 $MoviesArray = [$CasinoRoyale,$Glass,$SpiderMan];
---
+++ Homework: 07.11.2022/Objects 1-3/07-Dog/index.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "Dog.php";
+
+$lady =new Dog('Lady', Dog::FEMALE);
+$sam =new Dog('Sam', Dog::MALE);
+$molly =new Dog('Molly', Dog::FEMALE);
+
+$rocky =new Dog('Rocky', Dog::MALE, $molly, $sam);
+
+$max = new Dog('Max', Dog::MALE, $lady, $rocky);
+
+$sparky =new Dog('Sparky', Dog::MALE);
+
+$buster =new Dog('Buster', Dog::MALE);
+
+$coco =new Dog('Coco', Dog::FEMALE, $molly, $buster);
+
+$dogs = [
+    $max,
+    $rocky,
+    $sparky,
+    $sam,
+    $lady,
+    $molly,
+    $coco
+];
+
+foreach ($dogs as $dog){
+    echo "{$dog->getName()} ({$dog->getGender()}) ";
+    if (!is_null($dog->getMother())) {
+        echo "Mother: " . $dog->getMother()->getName() . " ";
+    }
+    if (!is_null($dog->getFather())) {
+        echo "Father: " . $dog->getFather()->getName();
+    }
+    echo PHP_EOL;
+}
+
+
